20496131fa750703058fb06f0d83fdbe4974849d
akaashz <145892248+akaashz@users.noreply.github.com>
2026-06-01 19:20:38 +0530

    Add files via upload

---
+++ Drive_problems/Easy level/Q15.py
@@ -0,0 +1,6 @@
+base=int(input("Enter the base number: "))
+exponent=int(input("Enter the exponent: "))
+
+for i in range(exponent):
+    result=base**exponent
+print(result)
---
+++ Drive_problems/Easy level/Q9.py
@@ -0,0 +1,5 @@
+#to write the program that reads N and prints the square of every number from 1 to N
+
+N=int(input())
+for i in range(1,N+1):
+    print(i**2)
